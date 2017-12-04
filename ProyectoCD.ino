ca326c6e1009399d5e94c3c6c3caa7e714146e80
GaliLeo4444 <31259040+GaliLeo4444@users.noreply.github.com>
2017-12-04 15:44:59 -0300

    USART

--- ProyectoCD.ino
+++ ProyectoCD.ino
@@ -1,6 +1,8 @@
 #include <LiquidCrystal.h>
 #include <Keypad.h>
 #include <EEPROM.h>
+#include <avr/interrupt.h>
+#include <avr/io.h>
 
 
 
@@ -34,31 +36,49 @@ byte buzzer = A4;     // pin altavoz
 
 
 
-void setup()
-{
-  InitTimer1();
-  initTimer2();
-  pinMode(A0, OUTPUT);
-  digitalWrite(A0, HIGH);
-  pinMode(A1, OUTPUT);
-  digitalWrite(A1, HIGH);
-  pinMode (ledVerde, OUTPUT);
-  digitalWrite(ledVerde, LOW); 
-  pinMode (ledRojo, OUTPUT);
-  digitalWrite(ledRojo, HIGH); 
-  pinMode (buzzer, OUTPUT);
-  //digitalWrite(buzzer, HIGH);
-  Serial.begin(9600);
-  miTeclado.setDebounceTime(200);      //Para evitar rebote de tecla
-  for (int i=1; i <= 6; i++){
-      codigoSecreto[i] = EEPROM.read(i);    //Busca el pass desde la EEPROM
+//Inicializacion USART
+void USART_Init(){
+  cli();
+  /* Set baud rate */
+  UBRR0H = 0;
+  UBRR0L = 103;
+  // U2x1 = 1 para Double Speed Mode,   UBRR1L = 207 => BAUDRATE = 9600
+  UCSR0A &= ~(1 << U2X0);   //Síncrono??
+  UCSR0A &= ~(1<<FE0);
+  UCSR0A &= ~(1<<DOR0);
+  /* Enable receiver and transmitter */
+  UCSR0B |= (1 << RXEN0)|(1<<RXCIE0);
+  UCSR0B |= (1 << TXEN0);
+  UCSR0B &= ~(1 << TXCIE0);
+  UCSR0B &= ~(1 << UCSZ02);
+  /* Set frame format: 8data, 1stop bit */
+  UCSR0C |= (1<<UCSZ00)|(1<<UCSZ01);    //8 bit de data
+  UCSR0C &= ~(1<<USBS0);    //1stop bit
+  UCSR0C &= ~((1<<UMSEL01)|(1<<UMSEL00));   //Asíncrono
+  UCSR0C &= ~((1<<UPM01)|(1<<UPM00));   //Paridad disabled
+  sei();
+}
+
+
+void USART_Transmit(unsigned char data){  
+  /* Espera por si el buffer de transmición no está vacío */
+  while ( !( UCSR0A & (1<<UDRE0) ) );
+  /* Coloca el byte en el buffer de transmisión */
+  UDR0 = data;
+}
+
+
+ISR(USART_RX_vect){
+  //Rx interrupt
+  if (UCSR0A & ((1<<FE0)|(1<<DOR0))){   //Error de trama o data overrun
+    char basura = UDR0;
+    USART_Transmit('?');
   }
-  lcd.begin(16, 2);
-  lcd.setCursor(0, 0);
-  lcd.print("Introduzca clave");
-  lcd.setCursor(cursor_D, 1);
-  lcd.print(">");
-  GenerarTono(800, 400);
+  else {
+    if ((UCSR0A & (1<<RXC0))){    // Recepción completa. Leo el dato recibido y lo almaceno en un char
+      pulsacion = UDR0;
+    }
+  }
 }
 
 
@@ -101,10 +121,10 @@ void Pausa(unsigned int tiempo) {
 
 void initTimer2 () {
   //Fast PWM Mode & OCRA top & Toggle OC2A(PB[3]) on Compare Match
-  TCCR0A = 0;
-  TCCR0B = 0;
-  TCCR0A |= (1 << COM2A0) | (1 << WGM21) | (1 << WGM20);
-  TCCR0B |= (1 << WGM22);
+  TCCR2A = 0;
+  TCCR2B = 0;
+  TCCR2A |= (1 << COM2A0) | (1 << WGM21) | (1 << WGM20);
+  TCCR2B |= (1 << WGM22);
 }
 
 
@@ -115,11 +135,11 @@ void GenerarTono (unsigned int tono, unsigned int tiempo) {
   // F.tono = 16MHz/1024/(OCR2A+1)/2
   //OCR2A+1 = 16MHz/(tono*2048)
   OCR2A = byte((7812/tono)+1);
-  TCCR0B |= (1 << CS22) | (1 << CS20);
+  TCCR2B |= (1 << CS22) | (1 << CS20);
   while (t < tiempo){
     
   }
-  TCCR0B &= ~(1 << CS22) | ~(1 << CS20);  //Timer/Counter stopped
+  TCCR2B &= ~(1 << CS22) | ~(1 << CS20);  //Timer/Counter stopped
 }
 
 
@@ -148,7 +168,8 @@ void CambiarPass()
   lcd.setCursor(4, 1);
   lcd.print(">");
   for (int i=1; i <= 6; i++){
-      while (!hayCaracter ()){
+      pulsacion = miTeclado.getKey();
+      while (!pulsacion){
       }  
       codigoSecreto[i] = pulsacion;
       EEPROM.write(i, pulsacion);
@@ -166,12 +187,33 @@ void CambiarPass()
 }
 
 
-char hayCaracter () {
-  pulsacion = miTeclado.getKey() ;       //Leemos pulsacion
-  if (Serial.available()>0){            //Leemos desde BlueTooth
-     pulsacion = Serial.read();
+
+
+void setup()
+{
+  USART_Init();
+  InitTimer1();
+  initTimer2();
+  pinMode(A0, OUTPUT);
+  digitalWrite(A0, HIGH);
+  pinMode(A1, OUTPUT);
+  digitalWrite(A1, HIGH);
+  pinMode (ledVerde, OUTPUT);
+  digitalWrite(ledVerde, LOW); 
+  pinMode (ledRojo, OUTPUT);
+  digitalWrite(ledRojo, HIGH); 
+  pinMode (buzzer, OUTPUT);
+  //digitalWrite(buzzer, HIGH);
+  miTeclado.setDebounceTime(200);      //Para evitar rebote de tecla
+  for (int i=1; i <= 6; i++){
+      codigoSecreto[i] = EEPROM.read(i);    //Busca el pass desde la EEPROM
   }
-  return pulsacion;
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
+  lcd.print("Introduzca clave");
+  lcd.setCursor(cursor_D, 1);
+  lcd.print(">");
+  GenerarTono(800, 400);
 }
 
 
@@ -179,7 +221,8 @@ char hayCaracter () {
 
 void loop()
 {
-  if (hayCaracter()){
+  pulsacion = miTeclado.getKey();
+  if (pulsacion){
       num_caracter++;
       cursor_D++;               // incrementamos el cursor
       lcd.setCursor(cursor_D, 1);
@@ -198,10 +241,9 @@ void loop()
               digitalWrite(ledRojo, LOW);     //Apagamos el LED rojo
               digitalWrite(ledVerde,HIGH);    //Encendemos el verde
               delay(4000);                  //Cuanto o qué esperamos????
-              if (hayCaracter()){
-                  if (pulsacion == '*'){
+              pulsacion = miTeclado.getKey();
+              if (pulsacion == '*'){
                       CambiarPass();
-                  }
               }
               digitalWrite(ledVerde, LOW); 
               digitalWrite(ledRojo, HIGH);
